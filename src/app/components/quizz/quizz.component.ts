8f24f4fecad637f0b5e960ac1afb6d7f59c31845
Eryck Santos <dev.erycksantos@gmail.com>
2025-03-23 14:24:13 -0300

    Calculando e exibindo o resultado.

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -35,15 +35,12 @@ export class QuizzComponent implements OnInit {
 
       this.questionIndex = 0
       this.quesitonMaxIndex = this.questions.length
-
-      console.log(this.questionIndex)
-      console.log(this.quesitonMaxIndex)
     }
   }
 
   playesChoose(value: string) {
     this.answers.push(value)
-    console.log(this.answers)
+    this.nextStep()
   }
 
   async nextStep() {
@@ -52,7 +49,24 @@ export class QuizzComponent implements OnInit {
     if (this.quesitonMaxIndex > this.questionIndex) {
       this.questionSelected = this.questions[this.questionIndex]
     } else {
+      const finalAnswer: string = await this.checkResult(this.answers)
       this.finished = true
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results]
     }
+  }
+
+  async checkResult(answers: string[]) {
+
+    const result = answers.reduce((previous, current, index, arr) => {
+      if (
+          arr.filter(item => item === previous).length > 
+          arr.filter(item => item === current).length 
+      ) {
+          return previous
+      } else {
+          return current
+      }
+    })
+  return result
   }
 }
