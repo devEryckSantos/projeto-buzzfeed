9238c724382ed729265c527f0b9190bad232aa02
Eryck Santos <dev.erycksantos@gmail.com>
2025-03-23 13:22:07 -0300

    Criando as propriedades lógicas.

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quizz',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
-export class QuizzComponent {
+export class QuizzComponent  implements OnInit{
+ 
+  title: string = ''
 
+  questios: any
+  questionSelected: any
+
+  answer: string[] = []
+  answerSelected: string = ""
+  
+  questionIndex: number = 0
+  quesitonMaxIndex: number = 0
+
+  finished:boolean = false
+
+  constructor() { }
+
+  ngOnInit(): void {  }
 }
